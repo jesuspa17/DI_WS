da3996cb9faef3e7505b55cae6cfa65df7cdafd2
jesuspa17 <jpallares94.jp@gmail.com>
2015-11-25 22:25:44 +0100

    Ejercicio Menú

---
+++ Tema04_Menús/EjercicioMenuOpciones/app/src/main/java/com/dam/salesianostriana/di/ejerciciomenuopciones/MainActivity.java
@@ -0,0 +1,48 @@
+package com.dam.salesianostriana.di.ejerciciomenuopciones;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.BLACK);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+
+
+    }
+    Menu menu;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_personalizado, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.compartir) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
